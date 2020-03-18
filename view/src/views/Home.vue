fc040ed0e64c5a3476ad7e92cfd0d2c4f2f12617
tanacchi <q111026d@mail.kyutech.jp>
2020-03-18 18:30:00 +0900

    Add base interface of todo app

--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -2,5 +2,43 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Welcome to Your Vue.js App</h1>
+    <input v-model="newitem_title"
+           @keypress.enter="add_item">
+    <p>{{ newitem_title }}</p>
+    <ul>
+      <li v-for="item in items"
+          :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        newitem_title: '',
+        newitem_id: 3,
+        items: [
+          {
+            id: 1,
+            title: "Todo 1"
+          },
+          {
+            id: 2,
+            title: "Todo 2"
+          }
+        ]
+      }
+    },
+    methods: {
+      add_item: function() {
+        this.items.push({
+          id: this.newitem_id++,
+          title: this.newitem_title
+        });
+        this.newitem_title = '';
+      }
+    }
+  }
+</script>
