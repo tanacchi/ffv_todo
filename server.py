d0aa2c49c99200f6938bba6464a368dd065e0d54
tanacchi <q111026d@mail.kyutech.jp>
2020-03-21 23:56:07 +0900

    Setup firebase cloud store on flask server

--- server.py
+++ server.py
@@ -4,16 +4,20 @@ from flask import (
 )
 from flask_cors import CORS
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
 app = Flask(__name__,
             template_folder='./dist',
             static_folder='./dist')
 CORS(app)
 
-#  @app.route('/<path:path>')
-#  @app.route('/', defaults={'path': ''})
-#  def root(path):
-    #  print("path", path)
-    #  return render_template('index.html')
+cred = credentials.Certificate('ffv-todo-app-firebase-adminsdk-4sdgu-9157c7cf91.json')
+firebase = firebase_admin.initialize_app(cred)
+db = firestore.client()
+db_items = db.collection(u'items')
 
 @app.route('/')
 def root():
@@ -25,6 +29,7 @@ def file(path):
 
 @app.route('/api/items')
 def todo_items():
+    #  items = db_items.stream()
     items = [
         { 'id': 1, 'title': "Todo Item 1", 'done': False },
         { 'id': 2, 'title': "Todo Item 2", 'done': True  }
