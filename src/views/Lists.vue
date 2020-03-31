6e1d98fee10a43351b5603a16a83a89cbe4a81db
tanacchi <q111026d@mail.kyutech.jp>
2020-03-31 13:23:34 +0900

    Add template of Lists page

---
+++ src/views/Lists.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="lists">
+    <p v-for="(list, id) in lists"
+           :key="id"
+           class="">
+      <span>{{ list.name }}</span>
+    </p>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+
+  const apiBaseURL = process.env.VUE_APP_API_BASEURL ? process.env.VUE_APP_API_BASEURL : 'http://localhost:5000';
+
+  export default {
+    data: function() {
+      return {
+        lists: {}
+      }
+    },
+    mounted: function() {
+      const lists_uri = apiBaseURL + 'api/lists';
+      axios.get(lists_uri)
+           .then(response => {
+             this.lists = response.data;
+           });
+      console.log("lists: " + this.lists);
+    }
+  }
+</script>
