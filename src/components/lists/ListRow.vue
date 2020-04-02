d13f84e83c5cc7db3f56c38121291b6f77921f96
tanacchi <q111026d@mail.kyutech.jp>
2020-04-02 23:36:52 +0900

    Add ListRow component

---
+++ src/components/lists/ListRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    <router-link
+      :to="linkPath">
+      {{ list_name }}
+    </router-link>
+  </p>
+</template>
+<script>
+  export default {
+    props: ['list_id', 'list_name'],
+    data: function() {
+      return {
+        linkPath: {
+          name: 'TodoList',
+          params: { id: this.list_id }
+        }
+      };
+    }
+  };
+</script>
--- src/views/Lists.vue
+++ src/views/Lists.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="lists">
     <new-list-form v-on:list-created="fetchList" />
-    <p v-for="(list, id) in lists"
-           :key="id"
-           class="">
-    <router-link
-      :to="{name: 'TodoList', params: {id: id}}">
-      {{ list.name }}
-    </router-link>
-    </p>
+    <list-row
+      v-for="(list, id) in lists"
+      :key="id"
+      :list_id="id"
+      :list_name="list.name">
+    </list-row>
   </div>
 </template>
 <script>
   import axios from 'axios'
   import NewListForm from "@/components/lists/NewListForm.vue"
+  import ListRow from "@/components/lists/ListRow.vue"
 
   const apiBaseURL = process.env.VUE_APP_API_BASEURL ? process.env.VUE_APP_API_BASEURL : 'http://localhost:5000';
 
   export default {
     components: {
       NewListForm,
+      ListRow,
     },
     data: function() {
       return {
